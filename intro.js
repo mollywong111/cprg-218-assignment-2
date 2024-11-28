8c444be42214d072bcd16276652c5e207db7c28a
mollywong111 <mollywong1989@gmail.com>
2024-11-27 19:24:00 -0700

    latest file uploaded

---
+++ intro.js
@@ -0,0 +1,45 @@
+intro.addEventListener("click", function(){
+console.log("Let me tell u sth about Moai.")
+
+title.innerHTML="Introduction";
+date.innerHTML=" "
+image.src="intro.jpg"
+description.innerHTML ='Moai statues are massive megaliths at Easter Island, well known as the Easter Island heads, were built in 1100 – 1600 A.D. by the natives Rapa Nui. Many people call them the Easter Island heads, but this is wrong. Photos often show the statues partly buried in soil, making them look like just heads. In reality, all these "heads" have full bodies. Here’s the story about this famous statues.';
+detail.style.backgroundColor="#FCDD9D";
+  
+});
+
+first.addEventListener("click", function(){
+console.log("How was Moai created by human?")
+
+
+  title.innerHTML="The Creation of Moai";
+  date.innerHTML="1100-1600 AD";
+  image.src="1100.jpg"
+  description.innerHTML ='There are about 1,000 Moai statues at Easter Island, some weighing up to 86 tons and standing 10 meters tall. Around 95% of the statues were carved from the volcano Rano Raraku. This spot was chosen because it has a lot of tuff, a type of soft volcanic rock. Tuff is easy to carve, which was important since the natives didn’t have metal tools and used stone tools called Toki instead.';
+  detail.style.backgroundColor="#D9B99F";
+    
+  });
+
+second.addEventListener("click", function(){
+console.log("Moaiiiii")
+
+
+    title.innerHTML="Cultural Collapse of Moai";
+    date.innerHTML="1600-1800s";
+    image.src="1600.jpg"
+    description.innerHTML ='When the first European ship reached Easter Island in 1722, all the statues were still standing. Over the years, visitors observed more statues falling, and by the late 1800s, none were left standing. The most widely accepted theory is that the statues were intentionally pushed over during tribal conflicts to dishonor rival groups. This idea is supported by the fact that most statues fell face-down into the ground. Another legend speaks of Nuahine Pīkea Uri, a woman with powerful mana (spiritual energy). In her anger, she caused the statues to fall when her four children left her without food. Some elders on Easter Island continue to believe this story.';
+    detail.style.backgroundColor="#F2D7D3";
+      
+    });
+
+third.addEventListener("click", function(){
+  console.log("Save Moai!!!")
+
+      title.innerHTML="Rediscovery and Restoration";
+      date.innerHTML="1800-Present";
+      image.src="1800.webp"
+      description.innerHTML ='The Moai gained worldwide recognition in the 19th and 20th centuries. Restoration efforts started in the mid-20th century, with some statues being re-erected on their original platforms. In 1995, Easter Island was designated a UNESCO World Heritage Site, and the Moai now stand as symbols of resilience and cultural pride for the Rapa Nui people.';
+      detail.style.backgroundColor="#ABCBCA";   
+      });
+
